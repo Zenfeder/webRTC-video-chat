52c4c54710b3fae7097df93eb03e867620c4c5e0
CG <1054609489@qq.com>
2020-06-21 01:45:19 +0800

    websocket 基本信令搭建完毕；webRTC 对等连接建立成功

---
+++ public/scripts/client.js
@@ -0,0 +1,158 @@
+(function() {
+  var boxMe = document.getElementById('box-me');
+  var boxInvite = document.getElementById('box-invite');
+  var inputMe = document.getElementById('input-me');
+  var inputReceiver = document.getElementById('input-receiver');
+  var btnConfirm = document.getElementById('btn-confirm');
+  var btnInvite = document.getElementById('btn-invite');
+  var btnAcceptInvitation = document.getElementById('btn-accept');
+  var btnRejectInvitation = document.getElementById('btn-reject');
+  // 我的信息（别忘了我有可能是发送者，也可能是接收者）
+  var myInfo = { nickname: '', socketId: '' };
+  // 接收者的信息
+  var receiverInfo = { nickname: '', socketId: '' };
+  // 发送者的信息
+  var senderInfo = { nickname: '', socketId: '' };
+  
+
+  var socket = io();
+
+  // 用户给自己取昵称了
+  btnConfirm.addEventListener('click', function(event) {
+    var nickname = inputMe.value.trim();
+    if (!nickname) {
+      alert('请输入你的昵称');
+      return;
+    }
+    
+    socket.emit('create own nickname', nickname);
+  });
+
+  // 根据昵称邀请对方进行视频通话
+  btnInvite.addEventListener('click', function(event) {
+    var receiverName = inputReceiver.value.trim();
+    if (!myInfo.nickname) {
+      alert('请先输入你的昵称');
+      return;
+    }
+    if (!receiverName) {
+      alert('请输入对方的昵称');
+      return;
+    }
+
+    if (receiverName === myInfo.nickname) {
+      alert('不能跟自己聊天');
+      return;
+    }
+    
+    socket.emit('send a invitation', receiverName);
+  });
+
+  socket.on('create own nickname success', function(nickname) {
+    boxMe.innerHTML = `创建成功，你的昵称为：<span style="color:red">${nickname}</span>`;
+    myInfo.nickname = nickname;
+    myInfo.socketId = socket.id;
+  });
+  socket.on('create own nickname error', function(nickname) {
+    var elemTips = document.createElement('p');
+    elemTips.style.color = 'red';
+    elemTips.innerHTML = `创建失败，昵称 <span style="font-weight:600">${nickname}</span> 已被占用`;
+    boxMe.appendChild(elemTips);
+    inputMe.value = '';
+    btnConfirm.setAttribute('disabled', true);
+    setTimeout(() => {
+      boxMe.removeChild(elemTips);
+      btnConfirm.removeAttribute('disabled');
+    }, 2000);
+  });
+
+  socket.on('invitation send success', function(receiver) {
+    var elemSenderHandle = document.getElementById('box-video-sender-handle');
+    document.getElementById('invite-form').style.display = 'none';
+    elemSenderHandle.style.color = 'green';
+    elemSenderHandle.innerHTML = '邀请发起成功，正在等待对方接听...';
+    receiverInfo.nickname = receiver.nickname;
+    receiverInfo.socketId = receiver.socketId;
+  });
+  socket.on('invitation send error', function(nickname) {
+    var elemTips = document.createElement('p');
+    elemTips.style.color = 'red';
+    elemTips.innerHTML = `邀请发起失败，用户 <span style="font-weight:600">${nickname}</span> 不存在`;
+    boxInvite.appendChild(elemTips);
+    setTimeout(() => {
+      boxInvite.removeChild(elemTips);
+    }, 2000);
+  });
+
+  socket.on('received an invitation', function(sender) {
+    var elemReceiverHandle = document.getElementById('box-video-receiver-handle');
+    var elemSenderMsg = document.getElementById('sender-msg');
+    elemReceiverHandle.style.display = 'block';
+    elemSenderMsg.innerHTML = `${sender.nickname} 邀请你视频聊天，是否接受？`;
+    senderInfo.nickname = sender.nickname;
+    senderInfo.socketId = sender.socketId;
+  });
+  
+  // 接受视频邀请
+  btnAcceptInvitation.addEventListener('click', function(event) {
+    socket.emit('accept an invitation', { sender: senderInfo, receiver: myInfo });
+    document.getElementById('box-video-receiver-handle').style.display = 'none';
+  });
+  // 拒绝视频邀请
+  btnRejectInvitation.addEventListener('click', function(event) {
+    socket.emit('reject an invitation', { sender: senderInfo, receiver: myInfo });
+    document.getElementById('box-video-receiver-handle').style.display = 'none';
+    senderInfo.nickname = '';
+    senderInfo.socketId = '';
+  });
+  
+  // 接收方（receiver）接受了你的视频通话邀请
+  socket.on('receiver accepted your invitation', function(receiver) {
+    var elemSenderHandle = document.getElementById('box-video-sender-handle');
+    elemSenderHandle.style.color = 'green';
+    elemSenderHandle.innerHTML = `${receiver.nickname} 接受了你的视频邀请`;
+
+    // 开始处理视频信息...
+    makeCall();
+  });
+
+  // 接收方（receiver）接受了你的视频通话邀请
+  socket.on('receiver rejected your invitation', function(receiver) {
+    var elemSenderHandle = document.getElementById('box-video-sender-handle');
+    document.getElementById('invite-form').style.display = 'block';
+    elemSenderHandle.style.color = 'red';
+    elemSenderHandle.innerHTML = `${receiver.nickname} 拒绝了你的视频邀请`;
+  });
+
+  /**
+   * webRTC 信令
+   * 建立对等连接
+   */
+  const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]};
+  // 发起方
+  async function makeCall() {
+    const peerConnection = new RTCPeerConnection(configuration);
+    socket.on('message', async message => {
+      if (message.answer) {
+        console.log('发送方收到接收方的 answer：', message);
+        const remoteDesc = new RTCSessionDescription(message.answer);
+        await peerConnection.setRemoteDescription(remoteDesc);
+      }
+    });
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    socket.emit('message with offer', { 'receiver': receiverInfo, 'offer': offer });
+  }
+
+  // 接收方
+  const peerConnection = new RTCPeerConnection(configuration);
+  socket.on('message', async message => {
+    if (message.offer) {
+      peerConnection.setRemoteDescription(new RTCSessionDescription(message.offer));
+      const answer = await peerConnection.createAnswer();
+      await peerConnection.setLocalDescription(answer);
+      console.log('接收方收到发送方的 offer：', message);
+      socket.emit('message with answer', { 'sender': senderInfo, 'answer': answer });
+    }
+  });
+})();
---
+++ server/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 3000;
+const publicPath = express.static(path.join(__dirname, '../public'));
+
+app.use('/', publicPath);
+
+app.get('/', (req, res) => {
+  res.sendFile(publicPath + '/index.html');
+});
+
+
+// 给自己取了昵称的 socket 队列
+const namedSocketQueue = [];
+
+io.on('connection', socket => {
+  console.log(`user (${socket.id}) connected`);
+
+  // 用户给自己取了昵称
+  socket.on('create own nickname', nickname => {
+    if (namedSocketQueue.every(elem => elem.nickname !== nickname)) {
+      console.log(`user (${socket.id}) create own nickname: ${nickname}`);
+      namedSocketQueue.push({ nickname, socketId: socket.id });
+      socket.emit('create own nickname success', nickname);
+    } else {
+      socket.emit('create own nickname error', nickname);
+    }
+  });
+
+  // 用户根据昵称邀请对方进行视频通话
+  socket.on('send a invitation', nickname => {
+    console.log(`user (${socket.id}) invite other by nickname: ${nickname}`);
+    var sender = namedSocketQueue.find(elem => elem.socketId === socket.id);
+    var receiver = namedSocketQueue.find(elem => elem.nickname === nickname);
+    if (receiver) {
+      socket.emit('invitation send success', receiver);
+      // 通知接收方（receiver），有人（sender）邀请他进行视频通话
+      io.to(receiver.socketId).emit('received an invitation', sender);
+    } else {
+      socket.emit('invitation send error', nickname);
+    }
+  });
+
+  // 接收方（receiver）接受了发起方（sender）的视频邀请
+  socket.on('accept an invitation', ({ sender, receiver }) => {
+    console.log(`${receiver.nickname} 接收了 ${sender.nickname} 的视频邀请`);
+    // 通知发起方（sender），对方（receiver）接受了他的视频通话邀请
+    io.to(sender.socketId).emit('receiver accepted your invitation', receiver);
+  });
+
+  // 接收方（receiver）接受了发起方（sender）的视频邀请
+  socket.on('reject an invitation', ({ sender, receiver }) => {
+    console.log(`${receiver.nickname} 拒绝了 ${sender.nickname} 的视频邀请`);
+    // 通知发起方（sender），对方（receiver）拒绝了他的视频通话邀请
+    io.to(sender.socketId).emit('receiver rejected your invitation', receiver);
+  });
+
+  /**
+   * webRTC 信令
+   * 建立对等链接
+   */
+  
+  // 对等链接信令的发起者发来的消息，需要传递给对等链接的接收者（receiver）
+  socket.on('message with offer', ({ receiver, offer }) => {
+    console.log(`发起者起对等链接，接收者为 ${receiver.nickname}, ${receiver.socketId}`);
+    io.to(receiver.socketId).emit('message', { offer });
+  });
+ 
+  // 对等链接信令的接收者发来的消息，需要传递给对等链接的发起者（sender）
+  socket.on('message with answer', ({ sender, answer }) => {
+    console.log(`接收者回复对等链接请求，发起者为 ${sender.nickname}, ${sender.socketId}`);
+    io.to(sender.socketId).emit('message', { answer });
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`user (${socket.id}) disconnected`);
+  });
+});
+
+
+http.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
+
+
